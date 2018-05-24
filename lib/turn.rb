61c5410964ccce49ac158c33f1a9a89e69e475a4
Hillevi Eklöw <hillevieklow@gmail.com>
2018-05-24 18:17:21 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,7 +8,8 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
     move(move, input, current_player="X")
-  else turn(board)
+  else 
+    turn(board)
   end
 
   display_board(board)
