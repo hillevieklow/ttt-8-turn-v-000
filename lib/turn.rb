56d128e7d4ed6d5dd14d6ac0c1b4d8cc4cd6629a
Hillevi Eklöw <hillevieklow@gmail.com>
2018-05-24 18:23:51 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,8 +4,7 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets.chomp
-  input.to_i
+  input = gets.to_i
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, input, current_player = "X")
