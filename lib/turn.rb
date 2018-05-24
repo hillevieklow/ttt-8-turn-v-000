332ccaa4b2e715c5f3bdf5cbdec57950218c3053
Hillevi Eklöw <hillevieklow@gmail.com>
2018-05-24 17:45:34 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,36 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} \n"
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.chomp
+  index = input_to_index(input)
+  if valid_move(board, index)
+  end
+
+
+end
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && index.between?(0,8)
+    true
+  else
+    false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    true
+  end
+end
+
+def move(board, index, char)
+end
